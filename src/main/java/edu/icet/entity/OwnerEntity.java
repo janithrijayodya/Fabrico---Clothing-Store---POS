e5239793a6a57f77039c1e4aeae06e2e4971034e
janithrijayodya <janithrijayodya@gmail.com>
2024-10-24 19:22:57 +0530

    completed signIn process

---
+++ src/main/java/edu/icet/entity/OwnerEntity.java
@@ -0,0 +1,16 @@
+package edu.icet.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OwnerEntity {
+    private String owner_id;
+    private String email;
+    private String password;
+}
